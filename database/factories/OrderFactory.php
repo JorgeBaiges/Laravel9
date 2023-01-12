093d008cc67e3141484c8f9d65076f6cbea7a8e3
Jorge Baiges <joosbaiges@gmail.com>
2023-01-12 16:25:41 +0000

    Migration Seeders Factorys 3

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            
+            "solicitante" => $this->faker->name(),
+            "fecha" => $this->faker->dateTime(),
+            "descripcion" => $this->faker->text(),       
+
+        ];
+    }
+}
---
+++ database/migrations/2023_01_12_155620_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+
+            $table->id();
+            $table->string("Nombre");
+            $table->integer("Nivel");
+            $table->integer("Horas academicas");
+            $table->string("ID Profesor");
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
